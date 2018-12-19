84ed507a919b3326f8bdd7baa26bfe2752d6b53a
Takanori <tknrizn@gmail.com>
2018-12-19 22:15:58 +0900

    create API frame

--- app.py
+++ app.py
@@ -6,23 +6,32 @@ Created on Wed Dec 19 20:56:06 2018
 @author: takanori
 """
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,jsonify
 import numpy as np
 
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
 
 
 @app.route('/')
 def init():
     return "CONNECTED"
 
+@app.route('/result')
+def result():
+    
+    keyword = request.args.get('keyword')
+    
+    res = {"result":keyword}
+    return jsonify(res)
 
 
 @app.route('/asyn')
 def asyn():
-    name = "FUGA"
-    title = "test"
+    name = "INPUT KEYWORD"
+    title = "word2vec"
     return render_template('index.html',name = name,title = title)
 
 
